eaa930fb4120e0be3fee35b991fe83337ca6aed4
Matyáš Matta <matyasmatta@email.cz>
2025-03-15 20:54:23 +0100

    hotfixed light mode icons home.html via darkmode.js

--- static/js/darkmode.js
+++ static/js/darkmode.js
@@ -11,6 +11,14 @@ document.addEventListener("DOMContentLoaded", function() {
             ? '/media/imgs/base/mark.png' 
             : '/media/imgs/base/mark-blk.png';
 
+        const micSrc = darkMode 
+            ? '/media/imgs/base/mic-light.png' 
+            : '/media/imgs/base/mic';
+
+        const goalSrc = darkMode 
+            ? '/media/imgs/base/goal-light.png' 
+            : '/media/imgs/base/goal.png';
+
         // Update the nav logo
         document.querySelectorAll('.nav-logo img').forEach(function(img) {
             img.src = logoSrc;
@@ -24,6 +32,16 @@ document.addEventListener("DOMContentLoaded", function() {
         // Update the mark logo
         document.querySelectorAll('.mark-img').forEach(function(img) {
             img.src = markSrc;
+        });
+
+        // Update the mic logo
+        document.querySelectorAll('.mic-img').forEach(function(img) {
+            img.src = micSrc;
+        });
+
+        // Update the mic logo
+        document.querySelectorAll('.goal-img').forEach(function(img) {
+            img.src = goalSrc;
         });
     }
 
